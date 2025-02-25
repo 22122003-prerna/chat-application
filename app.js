341e0332f6c674a9e57498459792492f15724895
Prerna <jainprerna156@gmail.com>
2025-02-25 23:34:01 +0530

    established a connection between a server and a client

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const app = express();
 const server = http.createServer(app)
 const io = new Server(server);
 io.on("connection", (socket)=>{
-    console.log("A new user has connected", socket.id)
+    socket.on("user-message", (message) => {
+        io.emit("message", message)
+    })
 })
 app.use(express.static(path.resolve("./public")));
 app.get("/", (req, res) => {
